d6d9a9b7e76dc443122b35e310e5df82a96e4aaa
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-03-27 22:55:02 -0300

    Sistema básca de conversaciones funcionando.

---
+++ SISTEMA CONVERSACIONES/chats.lua
@@ -0,0 +1,26 @@
+local M = {}
+M.lvl = 0;
+M[1] = {}
+M[1][1] = "chat 1 LVL 1"
+M[1][2] = "chat 2 LVL 1"
+M[1][3] = "chat 3 LVL 1"
+M[1][4] = "chat 4 LVL 1"
+------------------
+M[2] = {}
+M[2][1] = "chat 1 LVL 2"
+M[2][2] = "chat 2 LVL 2"
+M[2][3] = "chat 3 LVL 2"
+M[2][4] = "chat 4 LVL 2"
+------------------
+M[3] = {}
+M[3][1] = "chat 1 LVL 3"
+M[3][2] = "chat 2 LVL 3"
+M[3][3] = "chat 3 LVL 3"
+M[3][4] = "chat 4 LVL 3"
+------------------
+M[4] = {}
+M[4][1] = "chat 1 LVL 4"
+M[4][2] = "chat 2 LVL 4"
+M[4][3] = "chat 3 LVL 4"
+M[4][4] = "chat 4 LVL 4"
+return M;
