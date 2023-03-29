63b24e70d8da33103980f105d852a0d2fd4edc80
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-03-29 17:29:48 -0300

    Mapitas nuevos.

--- SISTEMA CONVERSACIONES/chats.lua
+++ SISTEMA CONVERSACIONES/chats.lua
@@ -7,20 +7,20 @@ M[1] = {}
 --M[1][4] = "chat 4 LVL 1"
 ------------------
 M[2] = {}
-M[2][1] = "chat 1 LVL 2"
-M[2][2] = "chat 2 LVL 2"
-M[2][3] = "chat 3 LVL 2"
-M[2][4] = "chat 4 LVL 2"
+--M[2][1] = "chat 1 LVL 2"
+--M[2][2] = "chat 2 LVL 2"
+--M[2][3] = "chat 3 LVL 2"
+--M[2][4] = "chat 4 LVL 2"
 ------------------
 M[3] = {}
-M[3][1] = "chat 1 LVL 3"
-M[3][2] = "chat 2 LVL 3"
-M[3][3] = "chat 3 LVL 3"
-M[3][4] = "chat 4 LVL 3"
+--M[3][1] = "chat 1 LVL 3"
+--M[3][2] = "chat 2 LVL 3"
+--M[3][3] = "chat 3 LVL 3"
+--M[3][4] = "chat 4 LVL 3"
 ------------------
 M[4] = {}
-M[4][1] = "chat 1 LVL 4"
-M[4][2] = "chat 2 LVL 4"
-M[4][3] = "chat 3 LVL 4"
-M[4][4] = "chat 4 LVL 4"
+--M[4][1] = "chat 1 LVL 4"
+--M[4][2] = "chat 2 LVL 4"
+--M[4][3] = "chat 3 LVL 4"
+--M[4][4] = "chat 4 LVL 4"
 return M;
