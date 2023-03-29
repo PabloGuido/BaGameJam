84e8ebd1c9d9b0ee32fdda57376476396da62b9e
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-03-29 20:52:07 -0300

    Avanzando con el sistema de puntos.

--- NIVELES/datos_nivel.lua
+++ NIVELES/datos_nivel.lua
@@ -1,6 +1,19 @@
 local M = {};
-M[2].puntos_necesarios = 1;
-M[2].puntos_obtenidos = 0;
+M.puntos_obtenidos = 0;
+--
+M[2] ={};
+M[2].puntos_necesarios = 2;
 
+M.chequeo_de_puntos = function()
+	if M.puntos_obtenidos == M[2].puntos_necesarios then
+		print("puntos necesarios conseguidos")
+	end
+end
+
+M.suma_resta_puntos = function(cuanto)
+	M.puntos_obtenidos = M.puntos_obtenidos + cuanto;
+	print("puntos obtenidos: " .. M.puntos_obtenidos)
+	M.chequeo_de_puntos();
+end
 
 return M;
