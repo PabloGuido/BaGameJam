c03652b2da96982c56f8e77f0b385300bb6ec056
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-03-29 15:51:06 -0300

    Algunas cosas para ayudar construir los mapas. A punto de pisar algunos mapas.

--- SISTEMA CONVERSACIONES/chats.lua
+++ SISTEMA CONVERSACIONES/chats.lua
@@ -1,10 +1,10 @@
 local M = {}
 M.lvl = 0;
 M[1] = {}
-M[1][1] = "chat 1 LVL 1"
-M[1][2] = "chat 2 LVL 1"
-M[1][3] = "chat 3 LVL 1"
-M[1][4] = "chat 4 LVL 1"
+--M[1][1] = "chat 1 LVL 1"
+--M[1][2] = "chat 2 LVL 1"
+--M[1][3] = "chat 3 LVL 1"
+--M[1][4] = "chat 4 LVL 1"
 ------------------
 M[2] = {}
 M[2][1] = "chat 1 LVL 2"
