5522b49fbc311eac0fe8dc176c2e097f17e9be80
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-03-29 21:21:39 -0300

    Chquea los puntos necesarios conseguidos para pasar el nivel. Faltar poner "win" cuando llega a la puerta.

--- NIVELES/datos_nivel.lua
+++ NIVELES/datos_nivel.lua
@@ -1,11 +1,21 @@
 local M = {};
 M.puntos_obtenidos = 0;
+M.jugando_nivel_numero = 1;
+--
+M[1] ={};
+M[1].puntos_necesarios = 2;
 --
 M[2] ={};
 M[2].puntos_necesarios = 2;
-
+--
+M[3] ={};
+M[3].puntos_necesarios = 5;
+--
+M[4] ={};
+M[4].puntos_necesarios = 3;
+--
 M.chequeo_de_puntos = function()
-	if M.puntos_obtenidos == M[2].puntos_necesarios then
+	if M.puntos_obtenidos == M[M.jugando_nivel_numero].puntos_necesarios then
 		print("puntos necesarios conseguidos")
 	end
 end
