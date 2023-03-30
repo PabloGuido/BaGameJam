fae8a7ba34f4f5d0475052e0ad1f9970c2234a29
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-03-30 01:45:22 -0300

    Reconocimiento automático de los interactuables necesarios.

--- NIVELES/datos_nivel.lua
+++ NIVELES/datos_nivel.lua
@@ -3,16 +3,16 @@ M.puntos_obtenidos = 0;
 M.jugando_nivel_numero = 1;
 --
 M[1] ={};
-M[1].puntos_necesarios = 2;
+M[1].puntos_necesarios = 0;
 --
 M[2] ={};
-M[2].puntos_necesarios = 2;
+M[2].puntos_necesarios = 0;
 --
 M[3] ={};
-M[3].puntos_necesarios = 5;
+M[3].puntos_necesarios = 0;
 --
 M[4] ={};
-M[4].puntos_necesarios = 3;
+M[4].puntos_necesarios = 0;
 --
 M.chequeo_de_puntos = function()
 	if M.puntos_obtenidos == M[M.jugando_nivel_numero].puntos_necesarios then
