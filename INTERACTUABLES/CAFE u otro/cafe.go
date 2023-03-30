025199eef6dc6e1ad0aaaf1e4fb364ba026b78b2
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-03-30 20:54:42 -0300

    Animación café.

--- INTERACTUABLES/CAFE u otro/cafe.go
+++ INTERACTUABLES/CAFE u otro/cafe.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/PLAYER/sprites/player.atlas\"\n"
-  "default_animation: \"inter\"\n"
+  "default_animation: \"cafe_off\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
