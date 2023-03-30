847fd0b59ad855e43fe0e7f39f0e0ba61000718b
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-03-30 15:03:02 -0300

    Para editar y probar.

--- NIVELES/datos_nivel.lua
+++ NIVELES/datos_nivel.lua
@@ -14,6 +14,32 @@ M[3].puntos_necesarios = 0;
 M[4] ={};
 M[4].puntos_necesarios = 0;
 --
+--
+M[5] ={};
+M[5].puntos_necesarios = 0;
+--
+M[6] ={};
+M[6].puntos_necesarios = 0;
+--
+M[7] ={};
+M[7].puntos_necesarios = 0;
+--
+M[8] ={};
+M[8].puntos_necesarios = 0;
+--
+--
+M[9] ={};
+M[9].puntos_necesarios = 0;
+--
+M[10] ={};
+M[10].puntos_necesarios = 0;
+--
+M[11] ={};
+M[11].puntos_necesarios = 0;
+--
+M[12] ={};
+M[12].puntos_necesarios = 0;
+--
 M.chequeo_de_puntos = function()
 	if M.puntos_obtenidos == M[M.jugando_nivel_numero].puntos_necesarios then
 		--print("chequeo_de_puntos necesarios conseguidos")
