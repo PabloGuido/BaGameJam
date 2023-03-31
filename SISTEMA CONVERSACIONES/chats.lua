725acb0997e82fef087330f8651ff132a23b119e
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-03-31 01:07:12 -0300

    Restart lvl funcionando a priori.

--- SISTEMA CONVERSACIONES/chats.lua
+++ SISTEMA CONVERSACIONES/chats.lua
@@ -1,72 +1,84 @@
 local M = {}
 M.lvl = 0;
 M[1] = {}
---M[1][1] = "chat 1 LVL 1"
---M[1][2] = "chat 2 LVL 1"
---M[1][3] = "chat 3 LVL 1"
+M[1].hacer_chat = true;
+M[1][1] = "chat 1 LVL 1"
+M[1][2] = "chat 2 LVL 1"
+M[1][3] = "chat 3 LVL 1"
 --M[1][4] = "chat 4 LVL 1"
 ------------------
 M[2] = {}
+M[2].hacer_chat = true;
 --M[2][1] = "chat 1 LVL 2"
 --M[2][2] = "chat 2 LVL 2"
 --M[2][3] = "chat 3 LVL 2"
 --M[2][4] = "chat 4 LVL 2"
 ------------------
 M[3] = {}
---M[3][1] = "chat 1 LVL 3"
---M[3][2] = "chat 2 LVL 3"
+M[3].hacer_chat = true;
+M[3][1] = "chat 1 LVL 3"
+M[3][2] = "chat 2 LVL 3"
 --M[3][3] = "chat 3 LVL 3"
 --M[3][4] = "chat 4 LVL 3"
 ------------------
 M[4] = {}
+M[4].hacer_chat = true;
 --M[4][1] = "chat 1 LVL 4"
 --M[4][2] = "chat 2 LVL 4"
 --M[4][3] = "chat 3 LVL 4"
 --M[4][4] = "chat 4 LVL 4"
 ------------------
 M[5] = {}
+M[5].hacer_chat = true;
 --M[2][1] = "chat 1 LVL 2"
 --M[2][2] = "chat 2 LVL 2"
 --M[2][3] = "chat 3 LVL 2"
 --M[2][4] = "chat 4 LVL 2"
 ------------------
 M[6] = {}
+M[6].hacer_chat = true;
 --M[3][1] = "chat 1 LVL 3"
 --M[3][2] = "chat 2 LVL 3"
 --M[3][3] = "chat 3 LVL 3"
 --M[3][4] = "chat 4 LVL 3"
 ------------------
 M[7] = {}
+M[7].hacer_chat = true;
 --M[4][1] = "chat 1 LVL 4"
 --M[4][2] = "chat 2 LVL 4"
 --M[4][3] = "chat 3 LVL 4"
 --M[4][4] = "chat 4 LVL 4"
 ------------------
 M[8] = {}
+M[8].hacer_chat = true;
 --M[2][1] = "chat 1 LVL 2"
 --M[2][2] = "chat 2 LVL 2"
 --M[2][3] = "chat 3 LVL 2"
 --M[2][4] = "chat 4 LVL 2"
 ------------------
 M[9] = {}
+M[9].hacer_chat = true;
 --M[3][1] = "chat 1 LVL 3"
 --M[3][2] = "chat 2 LVL 3"
 --M[3][3] = "chat 3 LVL 3"
 --M[3][4] = "chat 4 LVL 3"
 ------------------
 M[10] = {}
+M[10].hacer_chat = true;
 --M[4][1] = "chat 1 LVL 4"
 --M[4][2] = "chat 2 LVL 4"
 --M[4][3] = "chat 3 LVL 4"
 --M[4][4] = "chat 4 LVL 4"
 ------------------
 M[11] = {}
+M[11].hacer_chat = true;
 --M[2][1] = "chat 1 LVL 2"
 --M[2][2] = "chat 2 LVL 2"
 --M[2][3] = "chat 3 LVL 2"
 --M[2][4] = "chat 4 LVL 2"
 ------------------
 M[12] = {}
+M[12].hacer_chat = true;
 --M[3][1] = "chat 1 LVL 3"
 --M[3][2] = "chat 2 LVL 3"
 --M[3][3] = "chat 3 LVL 3"
