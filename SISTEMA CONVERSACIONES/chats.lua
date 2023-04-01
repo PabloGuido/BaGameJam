184c67bab2fde0d88fe893e79c12d3ef285864e9
HydriaSensus <hydriasensus@gmail.com>
2023-03-31 21:20:52 -0300

    Cuadro de texto arreglado

--- SISTEMA CONVERSACIONES/chats.lua
+++ SISTEMA CONVERSACIONES/chats.lua
@@ -2,31 +2,21 @@ local M = {}
 M.lvl = 0;
 M[1] = {}
 M[1].hacer_chat = true;
-M[1][1] = "¡Bienvenidos a la capacitación! Primero que nada, los felicito por 
-
-llegar hasta acá."
-M[1][2] = "Una de sus tareas va a ser encender todas las máquinas, voy a 
-
-mostrarles una para que vean cómo funcionan."
-M[1][3] = "Usa las flechas direccionales para desplazarte ←↨→"
+M[1][1] = "¡Bienvenidos a la capacitación! Primero que nada, los felicito por llegar hasta acá."
+M[1][2] = "Una de sus tareas va a ser encender todas las máquinas, voy a mostrarles una para que vean cómo funcionan."
+M[1][3] = "Usa las flechas direccionales para desplazarte"
 M[1][4] = "Diríguete a la SALIDA una vez resueltas todas las tareas."
 ------------------
 M[2] = {}
 M[2].hacer_chat = true;
-M[2][1] = "Sé que ya querran ponerse a trabajar, pero antes vamos a empezar con 
-
-unos ejercicios espaciales."
-M[2][2] = "La idea es seguir la señalética del piso para acostumbrarnos a los 
-
-recorridos y sectores de la oficina."
+M[2][1] = "Sé que ya querran ponerse a trabajar, pero antes vamos a empezar con unos ejercicios espaciales."
+M[2][2] = "La idea es seguir la señalética del piso para acostumbrarnos a los recorridos y sectores de la oficina."
 --M[2][3] = "chat 3 LVL 2"
 --M[2][4] = "chat 4 LVL 2"
 ------------------
 M[3] = {}
 M[3].hacer_chat = true;
-M[3][1] = "Muy bien, gente. Por un lado hubo un poco de ida y vuelta, pero creo 
-
-que el concepto quedó claro."
+M[3][1] = "Muy bien, gente. Por un lado hubo un poco de ida y vuelta, pero creo que el concepto quedó claro."
 M[3][2] = "Ahora sí, veamos cómo les va en un escenario más realista."
 --M[3][3] = "chat 3 LVL 3"
 --M[3][4] = "chat 4 LVL 3"
@@ -35,15 +25,9 @@ M[4] = {}
 M[4].hacer_chat = true;
 M[4][1] = "Perdón, pero ¿Puede ser que a mi no me estén dando ninguna máquina?"
 M[4][2] = "Bueno, les estamos asignando tareas según su perfil."
-M[4][3] = "Yo no me quiero meter, pero estudiamos juntes. Incluso ella tiene 
-
-mejores notas."
-M[4][4] = "No, no, me refiero al perfil psicológico. Pero podemos darle más 
-
-trabajo para que nos muestre su capacidad."
-M[4][5] = "Ya estoy acostumbrada a tener que trabajar más que el resto para 
-
-demostrarme."
+M[4][3] = "Yo no me quiero meter, pero estudiamos juntes. Incluso ella tiene mejores notas."
+M[4][4] = "No, no, me refiero al perfil psicológico. Pero podemos darle más trabajo para que nos muestre su capacidad."
+M[4][5] = "Ya estoy acostumbrada a tener que trabajar más que el resto para demostrarme."
 ------------------
 M[5] = {}
 M[5].hacer_chat = true;
@@ -54,14 +38,10 @@ M[5][3] = "Habría que reforzar más los ejercicios espaciales."
 ------------------
 M[6] = {}
 M[6].hacer_chat = true;
-M[6][1] = "Disculpe, pero no veo mucho trabajo en equipo con tanta división ¿No 
-
-habrá un espacio más equitativo?"
+M[6][1] = "Disculpe, pero no veo mucho trabajo en equipo con tanta división ¿No habrá un espacio más equitativo?"
 M[6][2] = "Me encanta esa actitud. Teamwork y liderazgo."
 M[6][3] = "Justo en la siguiente ambos tienen acceso a las computadoras."
-M[6][4] = "Igual que garantizar el acceso no implica que la infrastructura esté 
-
-diseñada de manera equitativa."
+M[6][4] = "Igual que garantizar el acceso no implica que la infrastructura esté diseñada de manera equitativa."
 M[6][5] = "No te preocupes, linda. Que acá el cupo laboral lo respetamos."
 ------------------
 M[7] = {}
@@ -69,12 +49,8 @@ M[7].hacer_chat = true;
 M[7][1] = "Quedaría una pruebita más, pero no se preocupen chicos, qu-"
 M[7][2] = "¿Podrías decir «chica», para referirte a mí también?"
 M[7][3] = "¿Prefieren que les diga, «chico y chica»?"
-M[7][4] = "De hecho, yo hace un tiempo empecé a considerarme como persona no-
-
-binarie"
-M[7][5] = "Uh, en realidad no necesitábamos tanta inclusión. Pero tranqui, que no 
-
-cambia nada."
+M[7][4] = "De hecho, yo hace un tiempo empecé a considerarme como persona no-binarie"
+M[7][5] = "Uh, en realidad no necesitábamos tanta inclusión. Pero tranqui, que no cambia nada."
 ------------------
 M[8] = {}
 M[8].hacer_chat = true;
@@ -82,9 +58,7 @@ M[8][1] = "Excelente, están totalmente calificados. Arrancan mañana mismo."
 M[8][2] = "Me parece que estamos sobre-calificades."
 M[8][3] = "Y estuvimos hablando, no nos gusta el ambiente de esta compañía."
 M[8][4] = "Decidimos rechazar la propuesta laboral."
-M[8][5] = "No, por favor, no me hagan esto. No saben lo difícil que es encontrar 
-
-y capacitar gente nueva."
+M[8][5] = "No, por favor, no me hagan esto. No saben lo difícil que es encontrar y capacitar gente nueva."
 M[8][6] = "Quizás quien necesite capacitarse más seas vos."
 M[8][7] = "..."
 ------------------
