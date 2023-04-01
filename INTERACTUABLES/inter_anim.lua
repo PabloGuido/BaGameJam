3d8de0a58837d57a7c4c90cc054062880f524142
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-04-01 08:16:14 -0300

    Detalle anim.

--- INTERACTUABLES/inter_anim.lua
+++ INTERACTUABLES/inter_anim.lua
@@ -6,7 +6,10 @@ M.anim_on = function(objetivo, escala, tiempo)
 end
 
 M.anim_on_gui = function(objetivo, escala, tiempo)
-	gui.animate(objetivo, "scale", escala, gui.EASING_INBACK, tiempo, 0, nil, gui.PLAYBACK_ONCE_BACKWARD);
+	gui.animate(objetivo, "scale", escala, gui.EASING_INBACK, tiempo, 0, 
+	function()
+		gui.set_scale(objetivo, vmath.vector4(1,1,1,1))
+	end, gui.PLAYBACK_ONCE_BACKWARD);
 end
 
 return M;
